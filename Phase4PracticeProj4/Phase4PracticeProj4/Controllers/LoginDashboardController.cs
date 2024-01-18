43daabd1925c9a1d509cf98a249e6e9948a03bc5
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2024-01-18 12:14:33 +0530

    practiceproj4p4

---
+++ Phase4PracticeProj4/Phase4PracticeProj4/Controllers/LoginDashboardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Phase4PracticeProj4.Controllers
+{
+    public class LoginDashboardController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "Ramana" && password == "ramana@345")
+            {
+                return RedirectToAction("Dashboard");
+            }
+            ViewBag.ErrorMessage = "Invalid username or password";
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Phase4PracticeProj4/Phase4PracticeProj4/Models/LoginPage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phase4PracticeProj4.Models
+{
+    public class LoginPage
+    {
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
